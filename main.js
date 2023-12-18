448448bdb6e6962370b20f43e7b4413f46166fb7
DrFontanaSantiago <sfontanaestudio@gmail.com>
2023-12-18 00:37:26 -0300

    se agrega function

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
+function agradecerUsuario(nombre, equipo) {
+    alert("¡Gracias, " + nombre + ", por compartir que tu sangre lleva los colores de " + equipo + "!");
+}
+
 let equipo;
+let nombre;
 
 while (true) {
+    nombre = prompt("Ingrese su nombre");
+
+    if (nombre === "" || nombre === null) {
+        alert("Por favor, ingrese un nombre válido.");
+        continue;
+    }
+
     equipo = prompt("Ingrese su equipo de fútbol");
 
     if (equipo === "" || equipo === null) {
-        break;
+        alert("Por favor, ingrese un equipo válido.");
+        continue;
     }
 
     if (equipo.toLowerCase() === "independiente") {
@@ -14,4 +27,6 @@ while (true) {
     } else {
         alert("No tienes 7 copas Libertadores");
     }
+
+    agradecerUsuario(nombre, equipo);
 }
